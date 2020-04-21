78a18bda7f4d7bb1a85f6c6952d9888462d90dc8
Peter Leung <pleung@hashicorp.com>
2020-04-21 10:27:33 -0400

    Add back required region

--- aws.tf
+++ aws.tf
@@ -14,9 +14,7 @@ variable "role_arn" {
 }
 
 provider "aws" {
-  
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.aws_region
 
   assume_role {
     role_arn     = var.role_arn
